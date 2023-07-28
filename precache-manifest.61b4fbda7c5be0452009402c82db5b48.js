a3f5f576b2a49d74af4d2efa43bc93950d11a550
issac2001 <issacthampi2001@gmail.com>
2023-07-29 01:57:21 +0530

    Updates

---
+++ precache-manifest.61b4fbda7c5be0452009402c82db5b48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06b2db8cb3a9b4a5d8b2",
+    "url": "/Netflix-clone/static/css/main.b716da90.chunk.css"
+  },
+  {
+    "revision": "06b2db8cb3a9b4a5d8b2",
+    "url": "/Netflix-clone/static/js/main.06b2db8c.chunk.js"
+  },
+  {
+    "revision": "403b39d58bbd2656f880",
+    "url": "/Netflix-clone/static/js/1.403b39d5.chunk.js"
+  },
+  {
+    "revision": "1b791d9469c4330de877",
+    "url": "/Netflix-clone/static/js/runtime~main.1b791d94.js"
+  },
+  {
+    "revision": "5ec65e410da56a691d0726d545f09c1b",
+    "url": "/Netflix-clone/index.html"
+  }
+];
